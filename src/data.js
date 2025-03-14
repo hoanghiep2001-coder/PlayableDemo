8c316a04decec65be3a373e79a3d5836ca91a8c7
HiepDH <hiepdh@ieccorp.vn>
2025-03-14 15:18:28 +0700

    update PA

---
+++ src/data.js
@@ -0,0 +1,235 @@
+
+const BestPA = [
+    {
+        timeline: "List:",
+        data: [
+            {
+                path: "/PATestDemo/FH_Playable_V15_03.html",
+                name: "FH_Playable_V15_03",
+                dev: "",
+            },
+            {
+                path: "/PATestDemo/FH_Playable_V15_04.html",
+                name: "FH_Playable_V15_04",
+                dev: "",
+            },
+            {
+                path: "/PATestDemo/ETA_Playable_V1_01.html",
+                name: "ETA_Playable_V1_01",
+                dev: "",
+            },
+            {
+                path: "/PATestDemo/SCJ_Playable_V4_01.html",
+                name: "SCJ_Playable_V4_01",
+                dev: "",
+            },
+            {
+                path: "/PATestDemo/FH_V14_Level_Valentine_2.html",
+                name: "FH_V14_Level_Valentine_2",
+                dev: "",
+            },
+            {
+                path: "/PATestDemo/FH_V14_Level_Valentine.html",
+                name: "FH_V14_Level_Valentine",
+                dev: "",
+            },
+            {
+                path: "/PATestDemo/FH_V14_Level_12.html",
+                name: "FH_V14_Level_12",
+                dev: "",
+            },
+            {
+                path: "/PATestDemo/FS_DoubleCone_FixCameraFOV.html",
+                name: "FS_DoubleCone_FixCameraFOV",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+            {
+                path: "/PATestDemo/Hole_Idea_Optimized.html",
+                name: "Hole_Idea_Optimized",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+            {
+                path: "/PATestDemo/SCJ_Playable_Cocos_V1_02_Fix.html",
+                name: "SCJ_Playable_Cocos_V1_02_Fix",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+            {
+                path: "/PATestDemo/SCJ_Playable_Cocos_V2_01.html",
+                name: "SCJ_Playable_Cocos_V2_01",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+
+            {
+                path: "/PATestDemo/FS_PA_Level_Double_Cone.html",
+                name: "FS_PA_Level_Double_Cone",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+            {
+                path: "/PATestDemo/FS_PA_Level_Single_Cone.html",
+                name: "FS_PA_Level_Single_Cone",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+
+            {
+                path: "/PATestDemo/FS_PA_Level_Single_Cone_TwoGoal.html",
+                name: "FS_PA_Level_Single_Cone_TwoGoal",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+            {
+                path: "/PATestDemo/FS_PA_Fix_Maggnet_Circle.html",
+                name: "FS_PA_Fix_Maggnet_Circle",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+            {
+                path: "/PATestDemo/FS_PA_Demo_Super_Pyramid_Level.html",
+                name: "FS_PA_Demo_Super_Pyramid_Level",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+            {
+                path: "/PATestDemo/FS_PA_Level_WonderWorld.html",
+                name: "FS_PA_Level_WonderWorld",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+            {
+                path: "/PATestDemo/VH_Playable_V2_01_Level_Double_Cone_processed.html",
+                name: "VH_Playable_V2_01_Level_Double_Cone_processed",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+            {
+                path: "/PATestDemo/VH_Playable_V2_02_Level_Single_Cone_processed.html",
+                name: "VH_Playable_V2_02_Level_Single_Cone_processed",
+                dev: "",
+                impression: "49.720",
+                CTR: "51.15%",
+                CVR: "8.57%",
+                IVR: "4.28%",
+                net: "Applovin",
+                updatedDate: "12 - 2023"
+            },
+        ]
+    },
+
+    // {
+    //     timeline: "Tháng 7:",
+    //     data: [
+    //         {
+    //             path: "/BestPA/Month7_2024/BattleDoll_Playable_MTG_Op1_GP_Click_PkMode.html",
+    //             name: "BattleDoll_Playable_MTG_Op1_GP_Click_PkMode",
+    //             dev: "Quân",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/Preview_Billard_Puzzle_TVC_Android_Playable_OP12_Drag_GP_LongFlowBasicTable.html",
+    //             name: "Preview_Billard_Puzzle_TVC_Android_Playable_OP12_Drag_GP_LongFlowBasicTable",
+    //             dev: "Phong",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/Preview_MakeoverASMR_Playable_Op35_Drag_FGP_BackMaker.html",
+    //             name: "Preview_MakeoverASMR_Playable_Op35_Drag_FGP_BackMaker",
+    //             dev: "Phong",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/Preview_MergeRobo_Playable_Unity_OP5_GP_FullFlow.html",
+    //             name: "Preview_MergeRobo_Playable_Unity_OP5_GP_FullFlow",
+    //             dev: "Phong",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/Satisday_Playable_Applovin_Op3_1_GP_Level12.html",
+    //             name: "Satisday_Playable_Applovin_Op3_1_GP_Level12",
+    //             dev: "Hiệp",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/Satisday_Playable_Applovin_Op3_GP_Level12.html",
+    //             name: "Satisday_Playable_Applovin_Op3_GP_Level12",
+    //             dev: "Hiệp",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/StickCarft_Playable_Applovin_Op6_GP_ChooseWeapon.html",
+    //             name: "StickCarft_Playable_Applovin_Op6_GP_ChooseWeapon",
+    //             dev: "Hiệp",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/StickCraft_Playable_Applovin_Op3_Click_GP_StickSonGoku.html",
+    //             name: "StickCraft_Playable_Applovin_Op3_Click_GP_StickSonGoku",
+    //             dev: "Hiệp",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/StickCraft_Playable_Applovin_OP4_Drag_GP_Fight.html",
+    //             name: "StickCraft_Playable_Applovin_OP4_Drag_GP_Fight",
+    //             dev: "Quân",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/StickCraft_Playable_Applovin_OP5_Click_FGP_Fight.html",
+    //             name: "StickCraft_Playable_Applovin_OP5_Click_FGP_Fight",
+    //             dev: "Quân",
+    //         },
+    //     ]
+    // },
+]
+
+export default BestPA;
